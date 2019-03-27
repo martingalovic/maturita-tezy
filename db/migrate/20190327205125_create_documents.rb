22f91356bba9e814001ed791686a0a7a7f6641a1
Martin Galovic <galovicmartin1@gmail.com>
2019-03-27 22:03:34 +0100

    updated migraiton

--- db/migrate/20190327205125_create_documents.rb
+++ db/migrate/20190327205125_create_documents.rb
@@ -1,7 +1,7 @@
 class CreateDocuments < ActiveRecord::Migration[5.2]
   def change
     create_table :documents do |t|
-      t.references :student
+      t.references :student, foreign_key: true
       t.boolean :checked, default: false
       t.string :file
       t.timestamps
