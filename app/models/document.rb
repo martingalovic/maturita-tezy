09951d992971e8f4cd6e3200f6eccd7931bdc91c
Martin Galovic <galovicmartin1@gmail.com>
2019-03-28 16:11:58 +0100

    optional checker

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,7 +2,7 @@ class Document < ApplicationRecord
   default_scope { order(created_at: :desc) }
 
   belongs_to :student
-  belongs_to :checker, {class_name: "Student"}
+  belongs_to :checker, {class_name: "Student"}, optional: true
 
   mount_uploader :file, Document::FileUploader
 end
