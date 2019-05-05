e3ce98de8d992c9dbc0d44cde6577fc2dabab0ff
Martin Galovic <galovicmartin1@gmail.com>
2019-05-05 20:37:58 +0200

    update bucker dir

--- app/uploaders/document/file_uploader.rb
+++ app/uploaders/document/file_uploader.rb
@@ -10,7 +10,7 @@ class Document::FileUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/tezy_#{Rails.env}/#{model.student_id}-#{model.student.name.parameterize}"
+    "uploads/tezy_#{Rails.env}#{ENV['BUCKET_DIR'].present? ? "_#{ENV['BUCKET_DIR'] : ''}/#{model.student_id}-#{model.student.name.parameterize}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
